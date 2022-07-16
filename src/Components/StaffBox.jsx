44f76e2578e53f09b6f9fd11bb892990954ed1c7
abhishek-T99 <timilsina.abhishek55@gmail.com>
2022-07-16 15:30:25 +0545

    fixed minor issues

--- src/Components/StaffBox.jsx
+++ src/Components/StaffBox.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import '../Styles/StaffBox.css'
 
-function StaffBox() {
+
+function StaffBox({ name, image, testimonial }) {
   return (
     <div className='staffbox pointer'>
-        <div className='icon'>
-                <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M2 16.1A5 5 0 0 1 5.9 20M2 12.05A9 9 0 0 1 9.95 20M2 8V6a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2h-6"></path><line x1="2" y1="20" x2="2" y2="20"></line></svg>
-            </div>
-            <div className='staff__meta'>
-                <h1 className='staff__text'>Staff Name</h1>
-                <p className='p staff__text p__color'>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis dolores provident minus aut cum alias.
-                </p>
-            </div>
+      <div className='staff__img__container'>
+        <img className='staff__img' src={image} alt="" />
+      </div>
+      <div className='staff__meta'>
+        <h1 className='staff__text'>{name}</h1>
+        <p className='p staff__text p__color'>
+          {testimonial}
+        </p>
+      </div>
     </div>
   )
 }
--- src/Components/Staffs.jsx
+++ src/Components/Staffs.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import '../Styles/Staffs.css'
 import StaffBox from './StaffBox'
+import staffImg1 from '../Images/profile-1.jpeg'
+
 
 function Staffs() {
   return (
@@ -23,13 +25,44 @@ function Staffs() {
         <div className='container'>
             <div className='row'>
                 <div className='col__3'>
-                    <StaffBox />
+                    <StaffBox 
+                        name='Staff 1'
+                        image={staffImg1}
+                        testimonial='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita numquam ipsam, consequuntur delectus 
+                        dolorem voluptas quasi est adipisci ut eveniet.'
+                    />
                 </div>
                 <div className='col__3'>
-                    <StaffBox />
+                    <StaffBox 
+                        name='Staff 2'
+                        image={staffImg1}
+                        testimonial='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita numquam ipsam, consequuntur delectus 
+                        dolorem voluptas quasi est adipisci ut eveniet.'
+                    />
                 </div>
                 <div className='col__3'>
-                    <StaffBox />
+                    <StaffBox 
+                        name='Staff 2'
+                        image={staffImg1}
+                        testimonial='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita numquam ipsam, consequuntur delectus 
+                        dolorem voluptas quasi est adipisci ut eveniet.'
+                    />
+                </div>
+                <div className='col__3'>
+                    <StaffBox 
+                        name='Staff 4'
+                        image={staffImg1}
+                        testimonial='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita numquam ipsam, consequuntur delectus 
+                        dolorem voluptas quasi est adipisci ut eveniet.'
+                    />
+                </div>
+                <div className='col__3'>
+                    <StaffBox 
+                        name='Staff 5'
+                        image={staffImg1}
+                        testimonial='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita numquam ipsam, consequuntur delectus 
+                        dolorem voluptas quasi est adipisci ut eveniet.'
+                    />
                 </div>
             </div>
         </div>
